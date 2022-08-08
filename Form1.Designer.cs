686cbb20db3258992e365550399b6a14eeba48da
Nikita Gvozdkov <gvozdkov.na@phystech.edu>
2022-08-08 21:55:40 +0300

    minor edits

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -235,6 +235,7 @@ partial class Form1
             this.ToExcelButton});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.toolStrip1.Size = new System.Drawing.Size(1323, 25);
             this.toolStrip1.TabIndex = 12;
             this.toolStrip1.Text = "toolStrip1";
